59ef2e37485a260a5ac969b66afef2a67dfd7ab6
John Manouse <johnmanouse@Macintosh-2.local>
2020-07-31 12:47:59 -0400

    data ouputs

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,13 +1,39 @@
-//function to generate repo name
-function generateRepoName(projectTitle) {
-  const repo = projectTitle
-}
-
 
 //function to generate readme information from responses
 function generateMarkdown(data) {
+  const NOT_PROVIDED = "Not Provided"
+
+
   return `
-# ${data.title}
+  # User Name
+# ${data.userName.length ? userName.title : NOT_PROVIDED}
+
+  # Title
+# ${data.projectName.length ? data.projectName : NOT_PROVIDED}
+
+# Description
+# ${data.projectDescription.length ? data.projectDescription.length : NOT_PROVIDED}
+
+# Table of Contents
+# ${data.tableContents.length ? data.tableContents.length : NOT_PROVIDED}
+
+# Installation Instructions
+# ${data.installInstructions.length ? data.installInstructions.length : NOT_PROVIDED}
+
+# Application Use Instructions
+# ${data.appInstructions.length ? data.appInstructions.length : NOT_PROVIDED}
+
+# Project Licenses
+# ${data.projectLicense.length ? data.projectLicense.length : NOT_PROVIDED}
+
+# Additional Contributions
+# ${data.projectContribute.length ? data.projectContribute.length : NOT_PROVIDED}
+
+# Project Testing
+# ${data.projectTesting.length ? data.projectTesting.length : NOT_PROVIDED}
+
+# Project email
+# ${data.emailAddress.length ? data.emailAddress.length : NOT_PROVIDED}
 
 `;
 }
